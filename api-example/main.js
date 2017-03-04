c1bd8aaa7bd2ebce4b74610d401c07c6f4374730
lucymonie <lucy.monie.hall@gmail.com>
2017-03-04 16:07:20 +0000

    Some small further changes to api example

--- api-example/main.js
+++ api-example/main.js
@@ -1,11 +1,11 @@
 (function () {
   var xhr = new XMLHttpRequest();
-  var url = "http://api.giphy.com/v1/gifs/search?q=funny+fall&api_key=dc6zaTOxFJmzC";
+  var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
   xhr.onreadystatchange = function() {
       if (xhr.readyState == 4 && xhr.status == 200) {
         var giphyObj = JSON.pars(xhr.responseText);
         var gifDrop = document.querySelector(".gif");
-        var link = giphyObj.data[0].images.downsized_medium.url;
+        var link = giphyObj.data[1].images.downsized_medium.url;
         gifDrop.src = link;
       }
   };
