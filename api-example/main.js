b94d2e6c4aaaafb1cb521eed49133dcf2bfe2450
Tony Lomax <antl.lomax@gmail.com>
2019-10-21 19:41:59 +0100

    Replace all vars with lets

--- api-example/main.js
+++ api-example/main.js
@@ -1,13 +1,14 @@
-(function () {
-  var xhr = new XMLHttpRequest();
-  var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+(function() {
+  let xhr = new XMLHttpRequest();
+  let url =
+    "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
   onreadystatechange = function() {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        var giphyObj = xhr.responseText;
-        var gifDrop = document.querySelector(".gif");
-        var link = giphyObj.data[1].images.downsized_medium.url;
-        gifDrop.src = link;
-      }
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let giphyObj = xhr.responseText;
+      let gifDrop = document.querySelector(".gif");
+      let link = giphyObj.data[1].images.downsized_medium.url;
+      gifDrop.src = link;
+    }
   };
   xhr.open("GET", url, true);
 })();
